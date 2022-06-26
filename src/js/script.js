0efa8a8a9b54468d57a451cfcdeec9e8d8ec518c
Karol Banat <karol.banat@gmail.com>
2022-06-26 15:47:45 +0200

    Subscription form handling

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const subscriptionForm = document.querySelector('.subscription');
+const emailInput = document.querySelector('.subscription__input');
+const submitBtn = document.querySelector('.submit-btn');
+const errorMsg = document.querySelector('.error-msg');
+
+const emailRegex =
+	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const handleSubmitButton = (e) => {
+	e.preventDefault();
+
+	if (!emailInput.value) {
+		subscriptionForm.classList.add('error');
+		errorMsg.innerText = 'Oops! Please add your email.';
+	} else if (!emailInput.value.match(emailRegex)) {
+		subscriptionForm.classList.add('error');
+		errorMsg.innerText = "Oops! That doesn't look like an email address.";
+	} else {
+		subscriptionForm.classList.remove('error');
+		errorMsg.innerText = '';
+	}
+};
+
+submitBtn.addEventListener('click', handleSubmitButton);
